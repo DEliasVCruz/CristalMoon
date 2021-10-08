aa411b46d12b36b0f5fbd79005810547f02540cb
Daniel Vilela <danielvilelayo@gmail.com>
2021-10-07 22:26:06 -0500

    fix(qf,git): disable number and statusline display

--- ftplugin/gitcommit.lua
+++ ftplugin/gitcommit.lua
@@ -1,2 +1,5 @@
 vim.opt_local.spell = true
 vim.opt_local.textwidth = 72
+vim.opt.laststatus = 1
+vim.opt.number = false
+vim.opt.relativenumber = false
--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,3 +1,5 @@
 require("utils.augroup").define_augroups({
 	_close_qf = { { "BufEnter", "<buffer>", "if winnr('$') < 2| q | endif" } },
 })
+
+vim.opt.colorcolumn = ""
