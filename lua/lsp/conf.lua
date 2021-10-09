fe517d680222817a0d89beeaa5d3a260f744a1c1
Daniel Vilela <danielvilelayo@gmail.com>
2021-10-09 14:03:49 -0500

    refactor(lsp): remove autocomand from lsp setup

--- lua/lsp/conf.lua
+++ lua/lsp/conf.lua
@@ -1,13 +1,6 @@
 local M = {}
 -- Async config of filetype and shada
 M.setup = function()
-  local func = require "utils.augroup"
-
-  func.define_augroups {
-    _general_settings = {
-      { "FileType", "*", "lua require('utils.ft').do_filetype(vim.fn.expand(\"<amatch>\"))" },
-    },
-  }
   vim.defer_fn(function()
     vim.cmd [[
                     if &ft == "packer" | echo "" | else | silent! e %
