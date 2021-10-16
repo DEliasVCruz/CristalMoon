a7ce6e4c622d73dbc50adbf423f5f8f7847eb6a9
DEliasVilelaC <danielvilelayo@gmail.com>
2021-10-16 12:17:02 -0500

    refactor(autocmd): remove ft lsp autocomand

--- lua/utils/autocomands.lua
+++ lua/utils/autocomands.lua
@@ -5,7 +5,6 @@ func.define_augroups {
     { "TextYankPost", "*", "lua require('vim.highlight').on_yank({higroup = 'YankHighlight', timeout = 200})" },
     { "FileType", "qf", "set nobuflisted" },
     { "BufWritePre", "*", "lua require('utils.functions').mkdir()" },
-    -- { "FileType", "*", "lua require('utils.ft').do_filetype(vim.fn.expand(\"<amatch>\"))" },
     { "VimResized", "*", "wincmd =" },
   },
   _formatting = {
