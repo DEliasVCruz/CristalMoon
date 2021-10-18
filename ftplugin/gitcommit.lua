a5f923eee257695b912a90bd644a3ba32ef11691
DEliasVilelaC <danielvilelayo@gmail.com>
2021-10-18 00:19:28 -0500

    feat(help, gitcommit): add quit with "q" keymap

--- ftplugin/gitcommit.lua
+++ ftplugin/gitcommit.lua
@@ -2,3 +2,4 @@ vim.opt_local.textwidth = 72
 vim.opt.laststatus = 1
 vim.opt.number = false
 vim.opt.relativenumber = false
+vim.api.nvim_buf_set_keymap(0, "n", "q", ":q!<cr>", { silent = true })
--- ftplugin/help.lua
+++ ftplugin/help.lua
@@ -1,1 +1,2 @@
 vim.opt_local.colorcolumn = ""
+vim.api.nvim_buf_set_keymap(0, "n", "q", ":q!<cr>", { silent = true })
