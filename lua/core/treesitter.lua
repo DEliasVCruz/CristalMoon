fac408a43ed5d221d29f8b57df82b27b9369f5e3
DEliasVilelaC <danielvilelayo@gmail.com>
2022-02-11 18:42:10 -0500

    refactor(treesitter): simplify treesitter config

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -1,34 +1,29 @@
+local status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+-- Treesitter Highlihgt module setting
+local default = {
+  ignore_install = {"markdown", "phpdoc"},
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<leader>as",
+      node_incremental = "<C-k>",
+      scope_incremental = "<leader>s",
+      node_decremental = "<C-j>",
+    },
+  },
+}
+
 local M = {}
 M.config = function()
-  -- Treesitter Highlihgt module setting
-  Treesitter = {
-    ensure_installed = {},
-    ignore_install = {"markdown"},
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    matchup = {
-      enable = true, -- mandatory, false will disable the whole extension
-    },
-    autotag = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<leader>as",
-        node_incremental = "<C-k>",
-        scope_incremental = "<leader>s",
-        node_decremental = "<C-j>",
-      },
-    },
-  }
-
-  local status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
-  if not status_ok then
-    return
-  end
-
-  treesitter_configs.setup(Treesitter)
+  treesitter_configs.setup(default)
 end
 
 return M
