a95ea5bf466c9293ac4d055c66a7032683bc6178
DEliasVilelaC <danielvilelayo@gmail.com>
2022-02-11 18:35:20 -0500

    refactor(colors): use new highlight api

--- lua/colors/vn_night.lua
+++ lua/colors/vn_night.lua
@@ -1,49 +1,67 @@
 require("vn-night").setup()
 
+local highlights = {
+  -- Highlight Yank
+  YankHighlight = { bg = "#b47109" },
+
+  -- Markdown support
+  Conceal = { fg = "#fe8019" },
+  pandocDelimitedCodeBlock = { fg = "#b9f27c" },
+  pandocNoFormatted = { fg = "#CF4F6D" },
+
+  -- Highligh Folding
+  Folded = { fg = "#96e2f0", bg = "none" },
+
+  -- Highligh for search
+  HlSearchFloat = { fg = "#2e222f", bg = "#fb6b1d" },
+  HlSearchLens = { fg = "#fb6b1d", bg = "none", bold = true, italic = true },
+
+  -- Whichkey colors
+  WhichKeyDesc = { link = "Keyword" },
+  WhichKeyGroup = { link = "Identifier" },
+
+  -- Qf colors
+  qfPath = { fg = "#FF5C8F" },
+  qfPosition = { fg = "#64657E" },
+  QuickFixLine = { fg = "none" },
+
+  -- Color column
+  ColorColumn = { bg = "none" },
+
+  -- Curosr Line Color
+  Cursorline = { bg = "none" },
+  Beacon = { bg = "white" },
+
+  -- Transparent background floating
+  NormalFloat = { bg = "#10081D" },
+  CmpDocWin = { bg = "none" },
+  Pmenu = { bg = "none" },
+  PmenuSbar = { bg = "none" },
+  PmenuThumb = { bg = "white" },
+  PmenuSel = { bold = true, fg = "#24283b", bg = "#b9f27c" },
+  CmpItemAbbrMatchFuzzyDefault = { fg = "#b9f27c" },
+  CmpItemAbbrMatch = { fg = "orange" },
+  CmpItemAbbr = { link = "Comment" },
+
+  -- Match brackets colors
+  MatchParen = { italic = true },
+
+  -- Galaxyline
+  StatusLine = { bg = "none" },
+  StatusLineNC = { bg = "none" },
+
+  -- Signature help
+  LspSignatureActiveParameter = { fg = "#f79617" },
+}
+
 -- Transperency
 vim.cmd [[hi SignColumn guibg=none]]
 vim.cmd [[hi CursorLineNR guibg=None]]
-vim.cmd [[highlight Normal guibg=none]]
-
--- Highlight Yank
-vim.cmd [[hi YankHighlight guibg=#b47109]]
-
--- Markdown support
-vim.cmd [[hi Conceal guifg=#fe8019]]
--- vim.cmd([[hi Conceal guifg=#a884f3]])
--- vim.cmd([[hi default link Conceal markdownRule]])
--- vim.cmd([[hi pandocDelimitedCodeBlock guifg=#a884f3]])
-vim.cmd [[hi pandocDelimitedCodeBlock guifg=#b9f27c]]
-vim.cmd [[hi pandocNoFormatted guifg=#CF4F6D]]
-
--- Highligh Folding
-vim.cmd [[highlight Folded guibg=none]]
-
--- Highligh for search
-vim.cmd [[hi HlSearchFloat guifg=#2e222f guibg=#fb6b1d]]
-vim.cmd [[hi HlSearchLens gui=bolditalic guifg=#fb6b1d guibg=NONE]]
-
--- Folds
-vim.cmd [[hi Folded guifg=#96e2f0 guibg=NONE]]
+vim.cmd [[hi Normal guibg=none]]
 
 -- Whichkey colors
-vim.cmd [[highlight WhichKeyFloat guibg=#10081D]]
-vim.cmd [[hi default link WhichKeyDesc Keyword]]
-vim.cmd [[hi default link WhichKeyGroup Identifier]]
+vim.cmd [[hi WhichKeyFloat guibg=none]]
 
--- Qf colors
-vim.cmd [[highlight qfPath guifg=#FF5C8F]]
-vim.cmd [[highlight qfPosition guifg=#64657E]]
-vim.cmd [[highlight QuickFixLine gui=bold guifg=none]]
-
--- Color column
-vim.cmd [[highlight ColorColumn guibg=#211337]]
-
--- Curosr Line Color
-vim.cmd [[highlight Cursorline guibg=#211337]]
-vim.cmd [[highlight Beacon guibg=white]]
-
--- Transparent background floating
-vim.cmd [[highlight NormalFloat guibg=none]]
--- Match brackets colors
-vim.cmd [[hi MatchParen cterm=italic gui=italic]]
+for group, options in pairs(highlights) do
+  vim.api.nvim_set_hl(0, group, options)
+end
