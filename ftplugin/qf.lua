eb0f4d5a9b59ace951e94abddc6b5e7be9b692c0
DEliasVilelaC <danielvilelayo@gmail.com>
2022-03-21 21:20:05 -0500

    refactor(fq): stop relying on quick fix

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,8 +1,9 @@
 vim.opt.colorcolumn = ""
 vim.opt.signcolumn = "yes:1"
 vim.opt.number = false
+vim.opt.buflisted = false
 
-vim.keymap.set("n", "<leader>ar", function()
+--[[ vim.keymap.set("n", "<leader>ar", function()
   require("replacer").run()
-end, { silent = true, buffer = true })
+end, { silent = true, buffer = true }) ]]
 vim.keymap.set("n", "q", ":q!<cr>", { silent = true, buffer = true })
