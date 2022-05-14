a1b7f2e532e61f9332c719ff6e130211d532a426
DEliasVilelaC <danielvilelayo@gmail.com>
2022-05-14 12:29:34 -0500

    refactor(lsp): remove unnecesary on_attach option

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -52,12 +52,6 @@ M.attach = function(client, bufnr)
   local function buf_set_keymap(...)
     vim.api.nvim_buf_set_keymap(bufnr, ...)
   end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
   -- Setup lsp attach
   require("lsp-format").on_attach(client)
